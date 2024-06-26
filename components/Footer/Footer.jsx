83c9280fb6fb20d67b8fab6c509453af9c3cf5e7
Łukasz <lukaszkus77@gmail.com>
2024-06-26 03:12:02 +0200

    update

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -17,3 +17,23 @@ const Footer = () => {
 };
 
 export default Footer;
+
+// <ul className={classes.footer__icons}>
+// <li>
+//   <a
+//     href="https://www.facebook.com/nazwa"
+//     className="fab fa-facebook-f"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   ></a>
+// </li>
+
+// <li>
+//   <a
+//     href="https://www.instagram.com/twoja_nazwa/"
+//     className="fab fa-instagram"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   ></a>
+// </li>
+// </ul>
--- components/Main/Realizations.jsx
+++ components/Main/Realizations.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import classes from "./Realizations.module.scss";
 import Image from "next/image";
 import photo1 from "../../assets/image/hero2_640.jpg";
+import photo2 from "../../assets/image/seasun.jpg";
 import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa";
 import { FaReact } from "react-icons/fa";
@@ -16,9 +17,13 @@ const Realizations = () => {
       <h2>Realizacje</h2>
 
       <div className={classes.realizations__box}>
-        <div className={classes.realizations__boxOne}>
+        <div className={classes.realizations__boxOT}>
           <div className={classes.realizations__boxImg}>
-            <Image src={photo1} alt="Zdjęcie"></Image>
+            <Image
+              className={classes.realizations__img}
+              src={photo1}
+              alt="Zdjęcie"
+            ></Image>
             <Link
               href="https://www.kabetintleather.pl/"
               className={`${classes.uiBtn} ${classes.imageOverlay}`}
@@ -32,6 +37,29 @@ const Realizations = () => {
             <p>
               Usługi <span>przyciemniania szyb</span> samochodowych i
               <span> regeneracji tapicerki</span> skórzanej.
+            </p>
+          </div>
+        </div>
+        <div className={classes.realizations__boxOT}>
+          <div className={classes.realizations__boxImg}>
+            <Image
+              className={classes.realizations__img}
+              src={photo2}
+              alt="Zdjęcie"
+            ></Image>
+            <Link
+              href="https://www.seasunsarbinowo.pl/"
+              className={`${classes.uiBtn} ${classes.imageOverlay}`}
+            >
+              <span>Zobacz</span>
+            </Link>
+          </div>
+
+          <div className={classes.realizations__boxOne}>
+            <h3>Sea & Sun Sarbinowo</h3>
+            <p>
+              Nowo wybudowane, <span>klimatyzowane, komfortowe domki</span> na
+              wynajem w urokliwej części Sarbinowa
             </p>
           </div>
         </div>
