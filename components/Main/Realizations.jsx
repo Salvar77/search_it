cc08eb1630a6a6e1aaf454049a3282e9b58b2948
Łukasz <lukaszkus77@gmail.com>
2024-07-30 19:13:14 +0200

    update

--- components/Main/Realizations.jsx
+++ components/Main/Realizations.jsx
@@ -3,8 +3,20 @@ import classes from "./Realizations.module.scss";
 import Image from "next/image";
 import photo1 from "../../assets/image/hero2_640.jpg";
 import photo2 from "../../assets/image/seasun.jpg";
+import vcardFazar from "../../assets/image/vcard.svg";
+import Fazar from "../../assets/image/FazarSvg.webp";
+import { useState } from "react";
 
 const Realizations = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <section id="realizacje" className={classes.realizations}>
       <h2>Realizacje</h2>
@@ -52,11 +64,53 @@ const Realizations = () => {
             <h3>Sea & Sun Sarbinowo</h3>
             <p>
               Nowo wybudowane, <span>klimatyzowane, komfortowe domki</span> na
-              wynajem w urokliwej części Sarbinowa
+              wynajem
+            </p>
+          </div>
+        </div>
+
+        <div className={classes.realizations__boxOT}>
+          <div className={classes.realizations__boxImg}>
+            <Image
+              className={classes.realizations__img}
+              src={Fazar}
+              alt="Wizytówka firmy Fazar"
+            ></Image>
+            <button
+              className={`${classes.uiBtn} ${classes.imageOverlay}`}
+              onClick={openModal}
+            >
+              <span>Przejdź</span>
+            </button>
+          </div>
+
+          <div className={classes.realizations__boxOne}>
+            <h3>Fazar</h3>
+            <p>
+              Wizytówka dla <span>usług elektrycznych</span> i{" "}
+              <span>teletechnicznych</span>
             </p>
           </div>
         </div>
       </div>
+
+      {isModalOpen && (
+        <div className={classes.modal} onClick={closeModal}>
+          <div
+            className={classes.modalContent}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <span className={classes.close} onClick={closeModal}>
+              &times;
+            </span>
+            <Image
+              src={vcardFazar}
+              alt="Wizytówka Fazar"
+              className={classes.modalImage}
+            />
+          </div>
+        </div>
+      )}
     </section>
   );
 };
